ac19e4941f241b042d1eebb3c2f802f914d46b6c
Kiran Rao <kiran.rao@gainspan.com>
2013-11-22 14:06:55 +0530

    Updated package name

--- src/com/github/curioustechizen/soissues/customstyle/MainActivity.java
+++ src/com/github/curioustechizen/soissues/customstyle/MainActivity.java
@@ -1,10 +1,10 @@
 
-package com.example.customstyletrial;
+package com.github.curioustechizen.soissues.customstyle;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import com.github.curioustechizen.soissues.customstyle.R;
+
 
 public class MainActivity extends Activity {
 
